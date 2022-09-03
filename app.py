3cd7bb2f3acb589e89b0a7b15ba28b9c8fc86d9b
mb6ockatf <mb6ockatf@relay.firefox.com>
2022-09-03 19:28:20 +0300

    ok

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from os import path, mkdir, remove
+from os import mkdir, remove, chmod, rmdir
 from sys import argv
 from datetime import datetime as dt
-from base64 import encodebytes
 from shutil import make_archive
 from flask import Flask, request, render_template, send_file, flash, redirect, session, url_for
 from werkzeug.exceptions import HTTPException
 from Crypto.Cipher import AES
+from stat import S_IWRITE
+from shutil import rmtree
 
 
 def encrypt(data: bytes, key: bytes):
@@ -15,19 +16,20 @@ def encrypt(data: bytes, key: bytes):
 
 
 def decrypt(ciphertext: bytes, key: bytes, nonce: bytes):
-   cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
-   plaintext = cipher.decrypt(ciphertext)
-   return plaintext
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    plaintext = cipher.decrypt(ciphertext)
+    return plaintext
 
 
 app = Flask(__name__)
-app.secret_key = argv[1].encode()
+# app.secret_key = argv[1].encode()
+app.secret_key = "bhfsuidbsbndisbds"
 app.permanent_session_lifetime = False
 
 
-@app.route("/favicon.ico")
+"""@app.route("/favicon.ico")
 def favicon():
-    return redirect(url_for('static'), filename='favicon.ico')
+    return redirect(url_for('avicon.ico'))"""
 
 
 @app.route("/")
@@ -37,36 +39,40 @@ def main():
 
 @app.route("/crypto_form_handler", methods=["GET", "POST"])
 def crypto_form_handler():
-    c_time = str(dt.now()).replace(' ', '-')
-    mkdir(c_time, mode=0o777)
+    c_time = str(dt.now()).replace(' ', '-').replace(':', '-')
+    mkdir(str(c_time))
     uploaded_path = f"{c_time}/uploaded.bin"
     nonce_path = f"{c_time}/nonce.bin"
     result_path = f"{c_time}/result.bin"
-    encrypted_archieve = f"{c_time}/encrypted.tar"
-    file = request.files['selected'].save(uploaded_path)
-    file = open(uploaded_path, 'rb').read()
+    file = request.files['selected']
+    file.save(uploaded_path)
+    file = open(uploaded_path, 'rb')
+    file_contents = file.read()
     key = request.form['key'].encode()
     mode = request.form['crypt']
     if mode == 'encrypt':
-         result, nonce = encrypt(file, key)
-         with open(nonce_path, "wb") as file:
-             file.write(nonce)
-         with open(result_path, "wb") as file:
-             file.write(result)
-         return send_file(make_archive("encrypted", "tar", c_time))
+        result, nonce = encrypt(file_contents, key)
+        with open(nonce_path, "wb") as opened:
+            opened.write(nonce)
+        with open(result_path, "wb") as opened:
+            opened.write(result)
+        result = send_file(make_archive("encrypted", "tar", str(c_time)))
     elif mode == 'decrypt':
-        nonce = request.files["nonce"].save(nonce_path)
+        nonce = request.files["nonce"]
+        nonce.save(nonce_path)
         with open(nonce_path, "rb") as stream:
             nonce = stream.read()
-        result = decrypt(file, key, nonce)
+        result = decrypt(file_contents, key, nonce)
         with open(result_path, "wb") as file:
             file.write(result)
-        return send_file(result_path)
+        result = send_file(result_path)
     else:
         flash('Режим не выбран')
-        return redirect(request.url)
+        result = redirect(request.url)
     file.close()
-    remove(c_time)
+    chmod(c_time, S_IWRITE)
+    rmtree(c_time, ignore_errors=True)
+    return result
 
 
 @app.route("/keys_form_handler", methods=["GET", "POST"])
@@ -89,5 +95,4 @@ def other_error_handler(error):
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
-
+    serve(app, host="127.0.0.1", port=8080)
